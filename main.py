ee83aa3371630c7c3cfa62afa5c12602f2bd9e81
dylanhamidy <dylanhamidy0909@gmail.com>
2024-04-11 10:21:57 +0000

    final: main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # initial version 
+
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    # YOUR ANSWER
+    num_indices = {}
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_indices:
+            return [num_indices[complement], index]
+        
+        num_indices[num] = index
+    
+    return []
+
+# Example input
+nums = [7, 11, 2, 15]
+target = 9
+
+# Call the function and print the result
+result = twoSum(nums, target)
+print("Output:", result)
